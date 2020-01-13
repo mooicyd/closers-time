04bcdb4fd8d371c1caadf4fa3db825b1bc6291f9
Dex Mooi <wtfrofllmao@gmail.com>
2020-01-13 17:10:21 +0900

    Major refactoring

--- bot.js
+++ bot.js
@@ -1,80 +1,82 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const v8 = require('v8');
+const schedule = require('node-schedule-tz');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 
     var raidchannel = client.channels.get(process.env.CHANNEL_ID);
 
-    var schedule = require('node-schedule-tz');
-    const startingHour = 15; const startHour = 16; const endingHour = 0; const endHour = 2;
-    const kovDays = [0,2,4,6]; const kofDays = [1,3,5,0]; const kofOffDays = [1,2,4,6]; const kovOffDays = [1,3,5,0];
-    //const korea = 'Asia/Seoul'
+    const RAID_STARTING_H = 15; const RAID_START_H = 16; const RAID_ENDING_H = 0; const RAID_END_H = 2; const TEN_HOUR = 36000000; const ONE_HOUR = 3600000; const TWO_HOUR = 7200000; const FIVE_MIN = 300000;
+    const KOV_DAYS = [0,2,4,6]; const KOF_DAYS = [1,3,5,0]; const NO_KOF_DAYS = [1,2,4,6]; const NO_KOV_DAYS = [1,3,5,0];
+    const KOV_RAID = "King of Voracity raid (LV87) "; const KOF_RAID = "King of Flies raid (LV85) "; const KOF_IMG = "\nhttps://imgur.com/PJVbqvz"; const KOV_IMG = "\nhttps://imgur.com/eVboHcQ";
+    const RAID_STARTING = "will open in 1 hour. Get Ready!"; const RAID_START = "has opened. The raid will remain open for the next 10 hours."; const RAID_ENDING = "will end in 2 hours.";
+    const RAID_END = "has ended."; const OVERFLOOD = "Overflood "; const OF_L_START = "has opened. The dungeon will remain open for 1 hour."; const OF_S_START = "has opened. The dungeon will remain open for 30 minutes".
 
     //King of Voracity Timers
     var kov_rule1 = new schedule.RecurrenceRule();
     kov_rule1.minute = 0;
-    kov_rule1.hour = startingHour;
-    kov_rule1.dayOfWeek = kovDays;
-    //kov_rule1.tz = korea;
+    kov_rule1.hour = RAID_STARTING_H;
+    kov_rule1.dayOfWeek = KOV_DAYS;
 
     var kov_rule2 = v8.deserialize(v8.serialize(kov_rule1));
-    kov_rule2.hour = startHour;
+    kov_rule2.hour = RAID_START_H;
 
     var kov_rule3 = v8.deserialize(v8.serialize(kov_rule2));
-    kov_rule3.hour = endingHour;
-    kov_rule3.dayOfWeek = kovOffDays;
+    kov_rule3.hour = RAID_ENDING_H;
+    kov_rule3.dayOfWeek = NO_KOV_DAYS;
 
     var kov_rule4 = v8.deserialize(v8.serialize(kov_rule3));
-    kov_rule4.hour = endHour;
+    kov_rule4.hour = RAID_END_H;
 
     //King of Flies Timers
     var kof_rule1 = v8.deserialize(v8.serialize(kov_rule1));
-    kof_rule1.dayOfWeek = kofDays;
+    kof_rule1.dayOfWeek = KOF_DAYS;
 
     var kof_rule2 = v8.deserialize(v8.serialize(kof_rule1));
-    kof_rule2.hour = startHour;
+    kof_rule2.hour = RAID_START_H;
 
     var kof_rule3 = v8.deserialize(v8.serialize(kof_rule2));
-    kof_rule3.hour = endingHour;
-    kof_rule3.dayOfWeek = kofOffDays;
+    kof_rule3.hour = RAID_ENDING_H;
+    kof_rule3.dayOfWeek = NO_KOF_DAYS;
 
     var kof_rule4 = v8.deserialize(v8.serialize(kof_rule3));
-    kof_rule4.hour = endHour;
+    kof_rule4.hour = RAID_END_H;
 
-    var kov_starting = schedule.scheduleJob(kov_rule1, function() {
-      raidchannel.send("King of Voracity raid (LV 87) will open in 1 hour. Get ready!");
-    });
+    //King of Voracity
 
-    var kov_start = schedule.scheduleJob(kov_rule2, function() {
-      raidchannel.send("King of Voracity raid (LV 87) has opened. The raid will remain open for the next 10 hours.\nhttps://imgur.com/eVboHcQ");
-    });
+    scheduleMessage(raidchannel, KOV_RAID, kov_rule1, RAID_STARTING, ONE_HOUR);
+    scheduleMessage(raidchannel, KOV_RAID, kov_rule2, RAID_START, TEN_HOUR, KOV_IMG);
+    scheduleMessage(raidchannel, KOV_RAID, kov_rule3, RAID_ENDING, TWO_HOUR);
+    scheduleMessage(raidchannel, KOV_RAID, kov_rule4, RAID_END, FIVE_MIN);
 
-    var kov_ending = schedule.scheduleJob(kov_rule3, function() {
-      raidchannel.send("King of Voracity raid (LV 87) will end in 2 hours.");
-    });
+    scheduleMessage(raidchannel, KOF_RAID, kof_rule1, RAID_STARTING, ONE_HOUR);
+    scheduleMessage(raidchannel, KOF_RAID, kof_rule2, RAID_START, TEN_HOUR, KOF_IMG);
+    scheduleMessage(raidchannel, KOF_RAID, kof_rule3, RAID_ENDING, TWO_HOUR);
+    scheduleMessage(raidchannel, KOF_RAID, kof_rule4, RAID_END, FIVE_MIN);
 
-    var kov_end = schedule.scheduleJob(kov_rule4, function() {
-      raidchannel.send("King of Voracity raid (LV 87) has ended.");
-    });
-
-    //King of Flies
-    var kof_starting = schedule.scheduleJob(kof_rule1, function() {
-      raidchannel.send("King of Flies raid (LV 85) will open in 1 hour. Get ready!");
-    });
-
-    var kof_start = schedule.scheduleJob(kof_rule2, function() {
-      raidchannel.send("King of Flies raid (LV 85) has opened. The raid will remain open for the next 10 hours.\nhttps://imgur.com/PJVbqvz");
-    });
-
-    var kof_ending = schedule.scheduleJob(kof_rule3, function() {
-      raidchannel.send("King of Flies raid (LV 85) will end in 2 hours.");
-    });
-
-    var kof_end = schedule.scheduleJob(kof_rule4, function() {
-      raidchannel.send("King of Flies raid (LV 85) has ended.");
-    });
+    clearMessages(raidchannel);
 });
 
 client.login(process.env.TOKEN);
+
+function scheduleMessage(channel, dungeon, rule, status, timeout, image) {
+    if(image === undefined) {
+        image = "";
+    }
+
+    var job = schedule.scheduleJob(rule, function () {
+        channel.send(dungeon + status + image).then(msg => msg.delete(timeout));
+    })
+}
+
+function clearMessages(channel) {
+    var cleanup_rule = new schedule.RecurrenceRule();
+    cleanup_rule.minute = 0;
+    cleanup_rule.hour = 4;
+
+    var cleanup = schedule.scheduleJob(cleanup_rule, function() {
+      channel.bulkDelete(100);
+  });
+}
