1cd9f84a34691fc77ed91e0cd75e208875dd8cfa
Dex Mooi <wtfrofllmao@gmail.com>
2020-03-18 21:22:34 +0900

    major refactor

--- bot.js
+++ bot.js
@@ -7,6 +7,14 @@ const time_regex = /([0-9]+)((:|.)([0-9]+))*/
 const time_delimit = /(:|.)/
 const {google} = require('googleapis');
 const sheets = google.sheets({version: 'v4'});
+const RAID_STARTING_H = 15; const RAID_START_H = 16; const RAID_ENDING_H = 0; const RAID_END_H = 2;
+const ONE_HOUR = 3600000; const TWO_HOUR = 7200000; const THIRTY_MIN = 1800000; const FIVE_MIN = 300000; const HALF_HOUR = 30;
+const KOV_DAYS = [0,2,4,6]; const KOF_DAYS = [1,3,5,0]; const NO_KOF_DAYS = [1,2,4,6]; const NO_KOV_DAYS = [1,3,5,0];
+const KOV_RAID = "King of Voracity raid (LV87) "; const KOF_RAID = "King of Flies raid (LV85) "; const KOF_IMG = "\nhttps://imgur.com/PJVbqvz"; const KOV_IMG = "\nhttps://imgur.com/eVboHcQ";
+const RAID_STARTING = "will open in 1 hour. Get Ready!"; const RAID_START = "has opened. The raid will remain open for the next 10 hours."; const RAID_ENDING = "will end in 2 hours.";
+const RAID_END = "has ended."; const OVERFLOOD = "Overflood "; const OF_L_START = "has opened. The dungeon will remain open for 1 hour."; const OF_S_START = "has opened. The dungeon will remain open for 30 minutes.";
+const OF_L_TIMES = [10, 15, 18, 20, 23]; const OF_S_TIMES = [0, 1]; const OF_IMG = "\nhttps://imgur.com/DrfvjPv"; const SEA_IMG = "\nhttps://i.imgur.com/IOzsjaZ.png";
+const SEA_DAYS = [3,6,0]; const SEA_START = "is open for today. The dungeon will be closed when this message is deleted."; const YOD_SEA = "Yod Sea (LV86) "; const WILL_OPEN = " will open today in 12 hours."
 
 var raidchannel;
 
@@ -16,68 +24,71 @@ client.on('ready', () => {
 
     raidchannel = client.channels.get(process.env.POST_CHANNEL_ID);
 
-    const RAID_STARTING_H = 15; const RAID_START_H = 16; const RAID_ENDING_H = 0; const RAID_END_H = 2;
-    const ONE_HOUR = 3600000; const TWO_HOUR = 7200000; const THIRTY_MIN = 1800000; const FIVE_MIN = 300000; const HALF_HOUR = 30;
-    const KOV_DAYS = [0,2,4,6]; const KOF_DAYS = [1,3,5,0]; const NO_KOF_DAYS = [1,2,4,6]; const NO_KOV_DAYS = [1,3,5,0];
-    const KOV_RAID = "King of Voracity raid (LV87) "; const KOF_RAID = "King of Flies raid (LV85) "; const KOF_IMG = "\nhttps://imgur.com/PJVbqvz"; const KOV_IMG = "\nhttps://imgur.com/eVboHcQ";
-    const RAID_STARTING = "will open in 1 hour. Get Ready!"; const RAID_START = "has opened. The raid will remain open for the next 10 hours."; const RAID_ENDING = "will end in 2 hours.";
-    const RAID_END = "has ended."; const OVERFLOOD = "Overflood "; const OF_L_START = "has opened. The dungeon will remain open for 1 hour."; const OF_S_START = "has opened. The dungeon will remain open for 30 minutes.";
-    const OF_L_TIMES = [10, 15, 18, 20, 23]; const OF_S_TIMES = [0, 1]; const OF_IMG = "\nhttps://imgur.com/DrfvjPv"; const SEA_IMG = "\nhttps://i.imgur.com/IOzsjaZ.png";
-    const SEA_DAYS = [3,6,0]; const SEA_START = "is open for today. The dungeon will be closed when this message is deleted."; const YOD_SEA = "Yod Sea (LV86) "; const WILL_OPEN = " will open today in 12 hours."
+    var kovStartingRule = new schedule.RecurrenceRule();
+    kovStartingRule.minute = 0;
+    kovStartingRule.hour = RAID_STARTING_H;
+    kovStartingRule.dayOfWeek = KOV_DAYS;
 
-    var kov_rule1 = new schedule.RecurrenceRule();
-    kov_rule1.minute = 0;
-    kov_rule1.hour = RAID_STARTING_H;
-    kov_rule1.dayOfWeek = KOV_DAYS;
+    var kovStartRule = copy(kovStartingRule);
+    kovStartRule.hour = RAID_START_H;
 
-    var kov_rule2 = v8.deserialize(v8.serialize(kov_rule1));
-    kov_rule2.hour = RAID_START_H;
+    var kovEndingRule = copy(kovStartRule);
+    kovEndingRule.hour = RAID_ENDING_H;
+    kovEndingRule.dayOfWeek = NO_KOV_DAYS;
 
-    var kov_rule3 = v8.deserialize(v8.serialize(kov_rule2));
-    kov_rule3.hour = RAID_ENDING_H;
-    kov_rule3.dayOfWeek = NO_KOV_DAYS;
-
-    var kov_rule4 = v8.deserialize(v8.serialize(kov_rule3));
-    kov_rule4.hour = RAID_END_H;
+    var kovEndRule = copy(kovEndingRule);
+    kovEndRule.hour = RAID_END_H;
 
     //King of Flies Timers
-    var kof_rule1 = v8.deserialize(v8.serialize(kov_rule1));
-    kof_rule1.dayOfWeek = KOF_DAYS;
+    var kofStartingRule = copy(kovStartingRule);
+    kofStartingRule.dayOfWeek = KOF_DAYS;
 
-    var kof_rule2 = v8.deserialize(v8.serialize(kof_rule1));
-    kof_rule2.hour = RAID_START_H;
+    var kofStartRule = copy(kofStartingRule);
+    kofStartRule.hour = RAID_START_H;
 
-    var kof_rule3 = v8.deserialize(v8.serialize(kof_rule2));
-    kof_rule3.hour = RAID_ENDING_H;
-    kof_rule3.dayOfWeek = NO_KOF_DAYS;
+    var kofEndingRule = copy(kofStartRule);
+    kofEndingRule.hour = RAID_ENDING_H;
+    kofEndingRule.dayOfWeek = NO_KOF_DAYS;
 
-    var kof_rule4 = v8.deserialize(v8.serialize(kof_rule3));
-    kof_rule4.hour = RAID_END_H;
+    var kofEndRule = copy(kofEndingRule);
+    kofEndRule.hour = RAID_END_H;
 
     //Overflood timers
-    var ofLong = setUpRules(hours=OF_L_TIMES);
+    var ofLong = setUpRules({hours: OF_L_TIMES});
 
     ofLong.forEach(function(rule) {
-        scheduleMessage(raidchannel, OVERFLOOD, rule, OF_L_START, ONE_HOUR, OF_IMG);
+        let messageObj = { channel: raidchannel, dungeon: OVERFLOOD, rule: rule, status: OF_L_START, timeout: ONE_HOUR, img: OF_IMG};
+        scheduleMessage(messageObj);
     })
 
-    var ofShort = setUpRules(hours=OF_S_TIMES, min=HALF_HOUR);
+    var ofShort = setUpRules({hours: OF_S_TIMES, min: HALF_HOUR});
 
     ofShort.forEach(function(rule) {
-        scheduleMessage(raidchannel, OVERFLOOD, rule, OF_S_START, THIRTY_MIN, OF_IMG);
+        let messageObj = { channel: raidchannel, dungeon: OVERFLOOD, rule: rule, status: OF_S_START, timeout: THIRTY_MIN, img: OF_IMG};
+        scheduleMessage(messageObj);
     })
 
     //King of Voracity
-    scheduleMessage(raidchannel, KOV_RAID, kov_rule1, RAID_STARTING, ONE_HOUR);
-    scheduleMessage(raidchannel, KOV_RAID, kov_rule2, RAID_START, NDEF, KOV_IMG);
-    scheduleMessage(raidchannel, KOV_RAID, kov_rule3, RAID_ENDING, TWO_HOUR);
-    scheduleMessage(raidchannel, KOV_RAID, kov_rule4, RAID_END);
+    var kov_starting = { channel: raidchannel, dungeon: KOV_RAID, rule: kovStartingRule, status: RAID_STARTING, timeout: ONE_HOUR};
+    var kov_start = { channel: raidchannel, dungeon: KOV_RAID, rule: kovStartRule, status: RAID_START, img: KOV_IMG};
+    var kov_ending = { channel: raidchannel, dungeon: KOV_RAID, rule: kovEndingRule, status: RAID_ENDING, timeout: TWO_HOUR, img: KOV_IMG};
+    var kov_end = { channel: raidchannel, dungeon: KOV_RAID, rule: kovEndingRule, status: RAID_END};
+
+    scheduleMessage(kov_starting);
+    scheduleMessage(kov_start);
+    scheduleMessage(kov_ending);
+    scheduleMessage(kov_end);
 
     //King of Flies
-    scheduleMessage(raidchannel, KOF_RAID, kof_rule1, RAID_STARTING, ONE_HOUR);
-    scheduleMessage(raidchannel, KOF_RAID, kof_rule2, RAID_START, NDEF, KOF_IMG);
-    scheduleMessage(raidchannel, KOF_RAID, kof_rule3, RAID_ENDING, TWO_HOUR);
-    scheduleMessage(raidchannel, KOF_RAID, kof_rule4, RAID_END);
+    var kof_starting = { channel: raidchannel, dungeon: KOF_RAID, rule: kofStartingRule, status: RAID_STARTING, timeout: ONE_HOUR};
+    var kof_start = { channel: raidchannel, dungeon: KOF_RAID, rule: kofStartRule, status: RAID_START, img: KOF_IMG};
+    var kof_ending = { channel: raidchannel, dungeon: KOF_RAID, rule: kofEndingRule, status: RAID_ENDING, timeout: TWO_HOUR, img: KOF_IMG};
+    var kof_end = { channel: raidchannel, dungeon: KOF_RAID, rule: kofEndingRule, status: RAID_END};
+
+    scheduleMessage(kof_starting);
+    scheduleMessage(kof_start);
+    scheduleMessage(kof_ending);
+    scheduleMessage(kof_end);
 
     clearMessages(raidchannel);
 
@@ -86,21 +97,24 @@ client.on('ready', () => {
     sea_rule.minute = 1;
     sea_rule.hour = 4;
     sea_rule.dayOfWeek = SEA_DAYS;
-    scheduleMessage(raidchannel, YOD_SEA, sea_rule, SEA_START, NDEF, SEA_IMG);
+    var sea_days = { channel: raidchannel, dungeon: YOD_SEA, rule: sea_rule, status: SEA_START, img: SEA_IMG}
+    scheduleMessage(sea_days);
 
     //KoF day
     var kof_day_rule = new schedule.RecurrenceRule();
     kof_day_rule.minute = 1;
     kof_day_rule.hour = 4;
     kof_day_rule.dayOfWeek = KOF_DAYS;
-    scheduleMessage(raidchannel, KOF_RAID, kof_day_rule, WILL_OPEN, NDEF, KOF_IMG);
+    var kof_days = { channel: raidchannel, dungeon: KOF_RAID, rule: kof_day_rule, status: WILL_OPEN, img: KOF_IMG};
+    scheduleMessage(kof_days);
 
     //KoV day
     var kov_day_rule = new schedule.RecurrenceRule();
     kov_day_rule.minute = 1;
     kov_day_rule.hour = 4;
     kov_day_rule.dayOfWeek = KOV_DAYS;
-    scheduleMessage(raidchannel, KOV_RAID, kov_day_rule, WILL_OPEN, NDEF, KOV_IMG);
+    var kov_days = { channel: raidchannel, dungeon: KOV_RAID, rule: kov_day_rule, status: WILL_OPEN, img: KOV_IMG};
+    scheduleMessage(kov_days);
 });
 
 //Custom Notification
@@ -139,10 +153,8 @@ client.on('message', async function(message) {
     if(message.channel.id === process.env.READ_CHANNEL_ID &&
         message.content.startsWith("<@!" + client.user.id + ">")) {
             var str = message.content.toLowerCase().replace("<@!" + client.user.id + ">", "").trim();
-            // client.channels.get(message.channel.id).send((await translate(str)));
-            await translate(str, message.channel.id)
+            await translate(str, message.channel.id);
         }
-
 });
 
 client.login(process.env.TOKEN);
@@ -198,23 +210,23 @@ async function translate(query, channelId) {
     };
 }
 
-function setUpRules(hours, min=0) {
+function setUpRules(obj) {
     var rules = new Array();
-    hours.forEach(function(value) {
+    obj.hours.forEach(function(value) {
         var rule = new schedule.RecurrenceRule();
-        rule.minute = min;
+        rule.minute = ((obj.min)? min: 0);
         rule.hour = value;
         rules.push(rule);
-    })
+    });
     return rules;
 }
 
-function scheduleMessage(channel, dungeon, rule, status, timeout=NDEF, image="") {
+function scheduleMessage(message) {
     var job = schedule.scheduleJob(rule, function () {
-        console.log(dungeon + status);
-        channel.send(dungeon + status + image).then(function(msg) {
-            if(timeout !== NDEF) {
-                msg.delete(timeout)
+        console.log(message.dungeon + message.status);
+        channel.send(`${message.dungeon}${message.status}${(message.image?message.image:"")}`).then(function(msg) {
+            if(message.timeout) {
+                msg.delete(timeout);
             }
         });
     })
@@ -235,4 +247,8 @@ function scheduleCustomNotify(channel, content, h, m) {
         channel.send(content);
     })
     return job;
+}
+
+function copy(obj) {
+    return v8.deserialize(v8.serialize(obj));
 }
